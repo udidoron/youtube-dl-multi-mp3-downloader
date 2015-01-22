f27b6527707f235910c353ca4d5c18cd271ead7a
udidoron <ehud.doron@mail.huji.ac.il>
2015-01-22 13:51:54 +0200

    Added initial (working) down.py and improved README.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # youtube-dl-multi-mp3-downloader
 Small wrapping script in Python (2.7), which allows downloading multiple YouTube files' audio into mp3 files. Uses youtube-dl, ffmpeg and ffprobe.
+
+Currently works only for Windows (sorry). Tested on Windows 7.
+
+Requirements
+====
+* youtube-dl.exe
+* ffmpeg.exe
+* ffprobe.exe
+
+(All in same folder)  
+Also, Python 2.7, either available via the PATH environment variable or via copying the program to the down.py location.
+
+ffmpeg and ffprobe can be downloaded together from same ffmpeg build.  
+I personally used Zeranoe FFmpeg:
+http://ffmpeg.zeranoe.com/builds/
+
+Usage instructions:
+===
+From the command line interface:  
+<code> down.py [links file]...[links file] </code>  
+Where a links file contains valid YouTube links, line-separated. At least one links file must be submitted to the script.
---
+++ down.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env
+
+import sys #for arguments
+import os #for running external program calls
+import re #for (minor) link processing
+
+
+# Confirming: we have at least one link file
+if len(sys.argv) < 2:
+    print "USAGE: down.py <links file> <possible other links files>"
+    exit(1)
+
+# Creating files directory
+dirname = "Youtube_MP3s"
+try:
+    os.mkdir(dirname)
+except OSError:
+    pass
+
+# Preparing regex for youtube ID
+idReg = re.compile("\?v=(.+)")
+
+# Reading links file and converting each link
+# Links file = one link per line
+linkFiles = sys.argv[1:] #disregarding first argument, which is the program name
+for linkfile in linkFiles:
+    print "Opening file "+str(linkfile)
+    for line in open(linkfile):
+        #getting youtube ID
+        youtubeID = idReg.search(line)
+        if youtubeID == None:
+            print "Invalid Youtube Link (no ID, i.e. no ?v=...)"
+            continue
+        downloadLink = "https://youtube.com/watch"+youtubeID.group(0)
+        print "Attempting to download "+str(downloadLink)
+        #downloading and converting file via external program
+        os.system("youtube-dl.exe --prefer-ffmpeg --extract-audio --audio-format mp3 "+downloadLink)
+        #moving converted file to folder
+        os.system("move *.mp3 "+dirname)
+
+
+print "All done!"
+
+
+
