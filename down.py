636eb9cfc745b6f2fea216c2abde63b5bdbbfa9e
udidoron <ehud.doron@mail.huji.ac.il>
2015-01-23 10:18:09 +0200

    Changed print statements to fit Python 3 as well

--- down.py
+++ down.py
@@ -7,7 +7,7 @@ import re #for (minor) link processing
 
 # Confirming: we have at least one link file
 if len(sys.argv) < 2:
-    print "USAGE: down.py <links file> <possible other links files>"
+    print("USAGE: down.py <links file> <possible other links files>")
     exit(1)
 
 # Creating files directory
@@ -24,22 +24,22 @@ idReg = re.compile("\?v=(.+)")
 # Links file = one link per line
 linkFiles = sys.argv[1:] #disregarding first argument, which is the program name
 for linkfile in linkFiles:
-    print "Opening file "+str(linkfile)
+    print("Opening file "+str(linkfile))
     for line in open(linkfile):
         #getting youtube ID
         youtubeID = idReg.search(line)
         if youtubeID == None:
-            print "Invalid Youtube Link (no ID, i.e. no ?v=...)"
+            print("Invalid Youtube Link (no ID, i.e. no ?v=...)")
             continue
         downloadLink = "https://youtube.com/watch"+youtubeID.group(0)
-        print "Attempting to download "+str(downloadLink)
+        print("Attempting to download "+str(downloadLink))
         #downloading and converting file via external program
         os.system("youtube-dl.exe --prefer-ffmpeg --extract-audio --audio-format mp3 "+downloadLink)
         #moving converted file to folder
         os.system("move *.mp3 "+dirname)
 
 
-print "All done!"
+print("All done!")
 
 
 
